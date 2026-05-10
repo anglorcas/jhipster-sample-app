69956685a837a5f297dd166e2fcae57baa7d405b
anglorcas <anglorcas@alum.us.es>
2026-05-10 15:50:06 +0200

    Modified tests counting script

--- scripts/count_tests.py
+++ scripts/count_tests.py
@@ -1,10 +1,15 @@
 import xml.etree.ElementTree as ET
 import glob
+
 total = 0
-for f in glob.glob('target/surefire-reports/TEST-*.xml'):
-    try:
-        root = ET.parse(f).getroot()
-        total += int(root.get('tests', 0))
-    except:
-        pass
+for pattern in [
+    'target/surefire-reports/TEST-*.xml',
+    'target/failsafe-reports/TEST-*.xml'
+]:
+    for f in glob.glob(pattern):
+        try:
+            root = ET.parse(f).getroot()
+            total += int(root.get('tests', 0))
+        except:
+            pass
 print(total)
